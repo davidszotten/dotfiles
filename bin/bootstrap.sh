e3354e055c861f00cdb662d385eb606ef3b5d2f5
David Szotten <david@odie.local>
2012-09-30 22:23:25 +0100

    update bootstrapper

---
+++ bin/bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+test -d ~/dotfiles || git clone git@github.com:davidszotten/dotfiles.git ~/dotfiles
+
+pushd ~/dotfiles
+git submodule init
+git submodule update
+
+function create_link {
+    # move any existing files (but overwrite existing symlinks)
+
+    TIMESTAMP=$(date +'%Y-%m-%d_%H:%M:%S')
+    src=$1
+    target=$2
+
+    if [[ -e "$HOME/$target" && ! -h "$HOME/$target" ]]
+    then
+        mv "$HOME/$target" "$HOME/$target$TIMESTAMP"
+    fi
+
+    ln -sf $"HOME/$1" "$HOME/$2"
+}
+
+create_link "dotfiles/ackrc"            ".ackrc"
+create_link "dotfiles/gitconfig"        ".gitconfig"
+create_link "dotfiles/gitignore"        ".gitignore"
+create_link "dotfiles/vim"              ".vim"
+create_link "dotfiles/vim/vimrc"        ".vimrc"
+create_link "dotfiles/vim/gvimrc"       ".gvimrc"
+create_link "dotfiles/vim/bash_profile" ".bash_profile"
+popd
+
+# build c extension for command-t if possible
+pushd ~/dotfiles/vim/bundle/command-t/ruby/command-t
+ruby extconf.rb
+make
+popd

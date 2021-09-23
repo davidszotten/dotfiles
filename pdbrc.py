6403cad949308e72228f9dadbafcc2a9bbad5967
David Szotten <davidszotten@gmail.com>
2021-09-23 15:24:24 +0100

    pdbpp history

--- pdbrc.py
+++ pdbrc.py
@@ -1,3 +1,6 @@
+import os.path
+
+
 try:
     from pdb import DefaultConfig, Color
 except ImporError:
@@ -7,3 +10,16 @@ else:
         sticky_by_default = True
         line_number_color = Color.darkgray
         current_line_color = 40
+
+        def setup(self, pdb):
+            import readline
+
+            histfile = os.path.expanduser("~/.pdb-history")
+            try:
+                readline.read_history_file(histfile)
+            except IOError:
+                pass
+            import atexit
+
+            atexit.register(readline.write_history_file, histfile)
+            readline.set_history_length(500)
